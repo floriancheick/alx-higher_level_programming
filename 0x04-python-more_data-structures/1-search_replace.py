52ff5afdda38354f728b9e11616b190c5cbb32c1
root <f.nganji@alustudent.com>
2022-12-08 15:35:37 -0800

    1-search_replace.py

--- 0x04-python-more_data-structures/1-search_replace.py
+++ 0x04-python-more_data-structures/1-search_replace.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)
+    if my_list is not None:
+        return [item if item != search else replace for item in my_list]
+    return None
