4fc539538a060f4e92ba0bc57eac9eca156878c7
root <f.nganji@alustudent.com>
2022-12-20 17:11:00 -0800

    2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    idex = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            idex += 1
+        except(TypeError, ValueError):
+            continue
+    print()
+    return idex
