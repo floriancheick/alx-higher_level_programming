69e91fd3ff35b7a0b4f0dddd0b559dff2ec35da2
root <f.nganji@alustudent.com>
2023-01-01 08:57:05 -0800

    4-print_square.py

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    """Print a square with the # character.
+"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
