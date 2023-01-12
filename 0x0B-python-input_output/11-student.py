cc2056f846f63c9e0ca3f02c78c5b9e269c74d99
root <f.nganji@alustudent.com>
2023-01-12 15:36:39 -0800

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module 11-student.py
+"""
+
+
+class Student:
+    """Class Definition"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes student with names and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """public method"""
+
+        if attrs is None:
+            return self.__dict__
+        else:
+            le = {}
+            for item in attrs:
+                if item in self.__dict__.keys():
+                    le[item] = self.__dict__[item]
+            return nary
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance from json"""
+
+        for key, value in json.items():
+            setattr(self, key, value)
